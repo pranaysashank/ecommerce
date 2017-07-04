26844a4a77abf7e85957e55fb6c68dfd3cc7ab48
Pranay Sashank <pranaysashank@gmail.com>
2017-07-04 20:43:12 +0530

    Created the personForm and made a basic form to create accounts

--- src/Handler/Register.hs
+++ src/Handler/Register.hs
@@ -1,9 +1,37 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE QuasiQuotes #-}
+
 module Handler.Register where
 
 import Import
+import Yesod.Form.Bootstrap3
+
+data Person = Person
+  { name :: Text
+  , mobileNumber :: Int
+  , email :: Maybe Text
+  , password :: Text
+  }
+  deriving Show
+
+personForm :: AForm Handler Person
+personForm = Person
+  <$> areq textField (bfs ("Your name" :: Text)) Nothing
+  <*> areq intField (bfs ("Mobile number" :: Text)) Nothing
+  <*> aopt textField (bfs ("Email address (optional)" :: Text)) Nothing
+  <*> areq passwordField (bfs ("Password" :: Text)) Nothing
 
 getRegisterR :: Handler Html
-getRegisterR = error "Route not defined : getRegisterR"
+getRegisterR = do
+  (widget, enctype) <- generateFormPost $ renderBootstrap3 BootstrapBasicForm personForm
+  defaultLayout $ do
+    $(widgetFile "register/register")
 
 postRegisterR :: Handler Html
-postRegisterR = error "Route not defined : postRegisterR"
+postRegisterR = do
+  ((res, widget), enctype) <- runFormPost $ renderBootstrap3 BootstrapBasicForm personForm
+  case res of
+    FormSuccess person -> defaultLayout [whamlet|<p>#{show person} |]
+    _ -> defaultLayout $(widgetFile "register/register")
