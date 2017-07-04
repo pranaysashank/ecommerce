81953d08ad50d32ae0c4b4e72ecb2e6e69913663
Pranay Sashank <pranaysashank@gmail.com>
2017-07-04 17:05:56 +0530

    Scaffolded Project

---
+++ src/Handler/Register.hs
@@ -0,0 +1,9 @@
+module Handler.Register where
+
+import Import
+
+getRegisterR :: Handler Html
+getRegisterR = error "Route not defined : getRegisterR"
+
+postRegisterR :: Handler Html
+postRegisterR = error "Route not defined : postRegisterR"
